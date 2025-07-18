059bafc7bbc5ea9ea5bbe7efa19b6b3706b53c24
CrozzDev <crozzdev95@outlook.com>
2025-07-17 21:55:05 -0500

    finished first exercise set3. Tests passed

---
+++ set3/fuel.py
@@ -0,0 +1,32 @@
+def main():
+    percentage = get_percentage()
+    print(percentage)
+
+
+def get_percentage() -> str:
+
+    percentage = ""
+    while True:
+        fraction = input("Fraction: ")
+
+        try:
+            x, y = fraction.split("/")
+            int_x, int_y = int(x), int(y)
+            if (int_x > int_y) or (int_x < 0 or int_y < 0):
+                raise ValueError
+            if int_x == int_y:
+                percentage = "F"
+            elif int_x == 0 and int_y != 0:
+                percentage = "E"
+            else:
+                percentage = f"{int((int_x / int_y) * 100)}%"
+
+        except ZeroDivisionError:
+            pass
+        except ValueError:
+            pass
+        else:
+            return percentage
+
+
+main()
