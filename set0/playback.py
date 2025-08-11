f46a6f833b1c7ea8d9e36d14b0889c309c493c9d
CrozzDev <crozzdev95@outlook.com>
2025-08-10 20:57:46 -0500

    fixed a typo

--- set0/playback.py
+++ set0/playback.py
@@ -2,4 +2,5 @@ def to_playback(phrase: str) -> str:
     """Replaces the whitespaces with '...'"""
     return phrase.replace(" ", "...")
 
-print(to_playback(input("")
+
+print(to_playback(input("")))
